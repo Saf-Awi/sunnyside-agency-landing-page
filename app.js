f2bafdfe83076f41df5d63a41ff7a6409be138fc
Safa Al-Hilo <116551942+Saf-Awi@users.noreply.github.com>
2023-07-20 13:44:51 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const menuBtn = document.querySelector('.hamburger-btn');
+const navLinks = document.getElementById('navLinks');
+
+menuBtn.addEventListener('click', function () {
+    navLinks.classList.toggle("show");
+    
+});
+
+
+
+  
+  
